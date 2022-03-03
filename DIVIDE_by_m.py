18c5340ef2ac6194830e13a6d77bd8f1cc9e2af6
KHUSHI PASTARIYA <73924781+khushipastariya@users.noreply.github.com>
2022-03-04 01:47:32 +0530

    Create DIVIDE_by_m.py

---
+++ DIVIDE_by_m.py
@@ -0,0 +1,12 @@
+# Python_lab_work__
+n= int(input("Enter n "))
+m=int(input("Enter m "))
+
+print()
+for i in range(1,n):
+    if (i%m==0):
+        print(i,end=" and ")
+        if(i%2==0):
+            print("Number is Even")
+        else:
+            print("Number is Odd")
